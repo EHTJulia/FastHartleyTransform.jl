dec22904eabc03139959fae81af97d969cfeeb66
Kazunori Akiyama <kakiyama@mit.edu>
2025-01-04 19:44:41 -0500

    first commit

--- src/FHT.jl
+++ src/FHT.jl
@@ -1,5 +1,25 @@
 module FHT
+import FFTW
+import AbstractFHTs: FHTPlan, 
+    plan_fht, plan_fht!, plan_ifht, plan_ifht!,
+    fht, fht!, ifht, ifht!,
+    *, inv, \,
+    eltype, size, length, ndims, 
+    fftdims, fftfreqs
+export plan_fht, plan_fht!, plan_ifht, plan_ifht!,
+    fht, fht!, ifht, ifht!,
+    *, inv, \,
+    eltype, size, length, ndims, 
+    fftdims, fftfreqs
 
-# Write your package code here.
+function plan_fht(A, dims; flags=FFTW.ESTIMATE, timelimit=Inf)
+    bfftplan = FFTW.plan_bfft(A, dims; flags=flags, timelimit=timelimit)
+    return FHTPlan(bfftplan)
+end
+
+function plan_fht!(A, dims; flags=FFTW.ESTIMATE, timelimit=Inf)
+    bfftplan = FFTW.plan_bfft(A, dims; flags=flags, timelimit=timelimit)
+    return FHTPlanInplace(bfftplan)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
 using FHT
+using StableRNGs
 using Test
 
 @testset "FHT.jl" begin
-    # Write your tests here.
+    rng = StableRNG(0)
+    x1d = rand(rng, Float64, 128)
+    x2d = rand(rng, Float64, 128, 128)
+
+    for x in (x1d, x2d)
+        # make FHT plan
+        p1 = plan_fht(x)
+        p2 = plan_fht!(x)
+
+        # check if the output is the same
+        y = p1 * x
+        @test y == p2 * deepcopy(x)
+
+        # make inverse FHT plan
+        ip1 = plan_ifht(x)
+        ip2 = plan_ifht!(x)
+        ip3 = inv(p1)
+        ip4 = inv(p2)
+
+        # make inverse FHT plan inplace
+        x2 = ip1 * y
+        @test x == x2
+        
+        # check if the plans are the same
+        @test x == ip3 * y 
+        @test x == ip2 * deepcopy(y)
+        @test x == ip4 * deepcopy(y)
+    end
 end

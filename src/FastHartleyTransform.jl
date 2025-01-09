90893e7d573605213f079012550b847b825963da
Kazunori Akiyama <kakiyama@mit.edu>
2025-01-08 20:35:11 -0500

    reflect changes in AbstractFastHourierTransform.jl

--- src/FastHartleyTransform.jl
+++ src/FastHartleyTransform.jl
@@ -1,8 +1,10 @@
 module FastHartleyTransform
-import FFTW: plan_bfft, ESTIMATE
+using FFTW: ESTIMATE, plan_r2r, plan_r2r!, plan_bfft, DHT
 import AbstractFastHartleyTransforms:
-    FHTPlan,
-    FHTPlanInplace,
+    BFFTPlan,
+    BFFTPlanInplace,
+    R2RPlan,
+    R2RPlanInplace,
     plan_fht,
     plan_fht!,
     plan_ifht,
@@ -36,14 +38,42 @@ export plan_fht,
     ndims,
     fftdims
 
-function plan_fht(A, dims; flags=ESTIMATE, timelimit=Inf)
-    bfftplan = plan_bfft(A, dims; flags=flags, timelimit=timelimit)
-    return FHTPlan(bfftplan)
+function plan_fht(
+    A::Array,
+    dims;
+    flags=ESTIMATE,
+    timelimit=Inf,
+    use_r2r::Bool=ndims(A) == 1 ? true : false,
+)
+    if use_r2r
+        if ndims(A) > 1
+            @warn "Multidimensional r2r transform does not provide the exact transform and likely be slower."
+        end
+        r2rplan = plan_r2r(A, DHT, dims; flags=flags, timelimit=timelimit)
+        return R2RPlan(r2rplan)
+    else
+        bfftplan = plan_bfft(A, dims; flags=flags, timelimit=timelimit)
+        return BFFTPlan(bfftplan)
+    end
 end
 
-function plan_fht!(A, dims; flags=ESTIMATE, timelimit=Inf)
-    bfftplan = plan_bfft(A, dims; flags=flags, timelimit=timelimit)
-    return FHTPlanInplace(bfftplan)
+function plan_fht!(
+    A::Array,
+    dims;
+    flags=ESTIMATE,
+    timelimit=Inf,
+    use_r2r::Bool=ndims(A) == 1 ? true : false,
+)
+    if use_r2r
+        if ndims(A) > 1
+            @warn "Multidimensional r2r transform does not provide the exact transform and likely be slower."
+        end
+        r2rplan = plan_r2r!(A, DHT, dims; flags=flags, timelimit=timelimit)
+        return R2RPlanInplace(r2rplan)
+    else
+        bfftplan = plan_bfft(A, dims; flags=flags, timelimit=timelimit)
+        return BFFTPlanInplace(bfftplan)
+    end
 end
 
 end

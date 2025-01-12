4a496742e8d23539d15c5774eb7d415caa038b93
Documenter.jl <documenter@juliadocs.github.io>
2025-01-12 16:43:37 +0000

    build based on f9f151d

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastHartleyTransform","category":"page"},{"location":"#FastHartleyTransform","page":"Home","title":"FastHartleyTransform","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastHartleyTransform.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastHartleyTransform]","category":"page"}]
+}
